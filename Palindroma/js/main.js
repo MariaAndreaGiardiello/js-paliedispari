a48762a8792b868e1dbf8bcb346840f62bdbc0b4
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-05-06 16:29:01 +0200

    bozza funzione per verifica parola

--- Palindroma/js/main.js
+++ Palindroma/js/main.js
@@ -1,2 +1,10 @@
 //Chiedere all’utente di inserire una parola
 //Creare una funzione per capire se la parola inserita è palindroma
+
+// 1. creo una funzione per genrare il controllo della parola
+function wordReverse(word) {
+    const reversedWord = word.reverse('');
+    return reversedWord;
+}
+
+// 2. chiedo all'utente di inserire una parola
